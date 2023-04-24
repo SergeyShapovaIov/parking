8432c6ae1b564a02d96eaed7aec042afb8ef40ff
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2023-04-24 13:15:21 +0300

    create api routes for car

---
+++ app/Http/Controllers/Api/CarController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    public function store()
+    {
+        
+    }
+    
+    public function getAll()
+    {
+        
+    }
+    
+    public function getByFilter()
+    {
+        
+    }
+    
+    public function deleteById()
+    {
+        
+    }
+    
+    public function deleteByOwnerId()
+    {
+        
+    }
+    
+    public function update()
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\Api\CarController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('cars')->group( function () {
+    Route::get('/', [CarController::class, 'getAll'])->name('api.car.get-all');
+    Route::get('/byFilter', [CarController::class, 'getByFilter'])->name('api.car.get-filter');
+    Route::post('/', [CarController::class, 'store'])->name('api.car.store');
+    Route::delete('/{id}', [CarController::class, 'deleteById'])->name('api.car.delete-by-id')->whereNumber('id');
+    Route::delete('/owner/{id}', [CarController::class, 'deleteByOwnerId'])->name('api.car.delete-by-owner-id');
+    Route::put('/{id}', [CarController::class, 'update'])->name('api.car.update');
 });
