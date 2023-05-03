2d13db6a85edeeeeeaa6b991e15ac30df3f36a0b
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2023-05-03 18:58:33 +0300

    change query, v_3

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -29,28 +29,49 @@ class Review extends Model
 //            ;
 
 //        return DB::table('review')
-//            ->join('buyer', 'buyer_id', 'buyer.id')
-//            ->join('product', "product_id", 'product.id')
 //            ->selectRaw('SUM(mark_helpful_review) AS all_mark_reviews, buyer_id')
 //            ->groupBy("buyer_id")
-//            ->where('mark_helpful_review', '>', 30)
+//            ->where('mark_helpful_review', '>', 10)
 //            ->pluck('buyer_id');
 
+//        return DB::table('review')
+//            ->selectRaw('SUM(mark_helpful_review) as all_mark_reviews, buyer_id')
+//            ->groupBy('buyer_id')
+//            ->get();
 
-        return DB::table('review')
-            ->join('buyer', 'buyer_id', 'buyer.id')
-            ->join('product', "product_id", 'product.id')
-            ->where('buyer_id', ' = ', function (Builder $query) {
-                $query->selectRaw('SUM(mark_helpful_review) AS all_mark_reviews, buyer_id')
-                    ->from('review')
-                    ->groupBy("buyer_id")
-                    ->where('mark_helpful_review', '>', 1000)
-                    ->pluck('buyer_id');
+//
+//        return DB::raw("")
 
-            })
-            ->where('city', '=', 'Волгоград')
-            ->orWhere('city', '=', 'Самара')
-            ->get();
-        
+
+        $sql = "select *
+from `review`
+         inner join `buyer` on `buyer_id` = `buyer`.`id`
+         inner join `product` on `product_id` = `product`.`id`
+where (`city` = 'Волгоград' or `city` = 'Самара')
+    and (`buyer_id` in (select `buyer_id`
+                        from ((select SUM(mark_helpful_review) AS all_mark_reviews, buyer_id
+                               from `review`
+                               group by `buyer_id`)) AS `table_2`
+                        where `all_mark_reviews` > 10))
+
+
+
+   or (`buyer_id` in (select `buyer_id`
+                      from (SELECT (`buyer_id`), COUNT(*) as `count_review`
+                            FROM (SELECT `buyer_id`, review.id
+                                  FROM review
+                                           INNER JOIN `product` ON `product_id` = `product`.`id`
+                                  WHERE price > 3000
+                                    and product_id in
+                                        (SELECT product_id
+                                         FROM (SELECT AVG(`points`) as 'avg_rating'
+                                               FROM rating
+                                                        INNER JOIN product on rating.product_id = product.id
+                                               WHERE price > 3000
+                                               group by `buyer_id`) as `table_4`
+                                         where 'avg_rating' > 4)) as `table_3`
+                            GROUP BY `buyer_id`) as `table_5`
+                      where `count_review` > 10));";
+
     }
 }
