3cc420c8c9770b67fca0675ca44da9b45c8af4ac
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2023-05-11 18:25:39 +0300

    added routes for address and recipient requests

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::prefix('v1')->group(function () {
+   Route::prefix('address')->group(function () {
+        Route::get('/', [AddressController::class, 'getAll'])->name('api.v1.address.getAll');
+        Route::get('/{id}', [AddressController::class, 'getById'])->name('api.v1.address.getById')->whereNumber('id');
+        Route::post('/', [AddressController::class, 'store'])->name('api.v1.address.store');
+        Route::put('/{id}', [AddressControler::class, 'updateById'])->name('api.v1.address.updateById')->whereNumber('id');
+        Route::delete('/{id}', [AddressController::class, 'deleteById'])->name('api.v1.address.deleteById')->whereNumber('id');
+   });
+   Route::prefix('recipient')->group(function () {
+       Route::get('/', [RecipientController::class, 'getAll'])->name('api.v1.recipient.getAll');
+       Route::get('/{id}', [RecipientController::class, 'getById'])->name('api.v1.recipient.getById')->whereNumber('id');
+       Route::post('/', [RecipientController::class, 'store'])->name('api.v1.recipient.store');
+       Route::put('/{id}', [RecipientController::class, 'updateById'])->name('api.v1.recipient.updateById')->whereNumber('id');
+       Route::delete('/{id}', [RecipientController::class, 'deleteById'])->name('api.v1.recipient.deleteById')->whereNumber('id');
+   });
+});
